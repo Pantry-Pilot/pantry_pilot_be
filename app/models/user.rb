54816b0a73e04bd963f93097c1aa0f4a284cf351
Antoine Aube <aaube3@gmail.com>
2023-10-31 16:11:38 -0600

    model: users model with validations and secure password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  validates :name, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :password_digest, presence: true
+
+  has_secure_password
 end
