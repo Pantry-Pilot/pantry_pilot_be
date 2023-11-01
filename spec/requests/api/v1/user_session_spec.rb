39bf9bfba8504f1aa5801868fed12474cdb78863
Antoine Aube <aaube3@gmail.com>
2023-11-01 08:33:08 -0600

    tests: update tests to have sad path for error responses to user login and create

--- spec/requests/api/v1/user_session_spec.rb
+++ spec/requests/api/v1/user_session_spec.rb
@@ -5,10 +5,10 @@ RSpec.describe "Create A User endpoint" do
     it "validate a user and send a successful response back" do
       user = User.create(name: "Guy", email: "guy@gmail.com", password: "password")
       post "/api/v1/login", params:  { email: "guy@gmail.com", password: "password", password_confirmation: "password"} 
-
+      
       expect(response).to be_successful
       expect(response.status).to eq(201)
-
+      
       response_body = JSON.parse(response.body, symbolize_names: true)
       
       expect(response_body[:data]).to be_a(Hash)
@@ -17,5 +17,18 @@ RSpec.describe "Create A User endpoint" do
       expect(response_body[:data][:attributes][:name]).to eq(user.name)
       expect(response_body[:data][:attributes][:email]).to eq(user.email)
     end
+  end
+  
+  it "returns an error if the user is not found" do
+    user = User.create(name: "Guy", email: "guy@gmail.com", password: "password")
+    post "/api/v1/login", params:  { email: "notguy@gmail.com", password: "notpassword", password_confirmation: "notpassword"} 
+
+    expect(response).to_not be_successful
+    expect(response.status).to eq(400)
+
+    response_body = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response_body[:error]).to eq("Invalid email or password")
+    expect(response_body[:status]).to eq(400)  
   end
 end
