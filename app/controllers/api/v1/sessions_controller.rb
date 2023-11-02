639d9556eaa1aa0d89f346797d450c3e0ed1b363
Antoine Aube <aaube3@gmail.com>
2023-11-02 11:11:51 -0600

    feature: session users show endpoint

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -4,10 +4,20 @@ class Api::V1::SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
 
     if user &.authenticate(params[:password])
-      session[:user_id] = user.id
       render json: UsersSerializer.new(user), status: 201
     else 
       render json: { error: "Invalid email or password", status: 400 }, status: 400
     end
   end 
+
+  def show
+    user = User.find(params[:id])
+    render json: UsersSerializer.new(user)
+  end
+
+  private
+
+  def user_params
+    params.permit(:name, :email, :password)
+  end
 end
