146e37df3c8ab62fa0b593bc1fe7ab05b26e0ed7
Antoine Aube <aaube3@gmail.com>
2023-11-01 18:03:48 -0600

    feature: user session made on the back end

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -2,7 +2,9 @@ class Api::V1::SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:email])
+
     if user &.authenticate(params[:password])
+      session[:user_id] = user.id
       render json: UsersSerializer.new(user), status: 201
     else 
       render json: { error: "Invalid email or password", status: 400 }, status: 400
