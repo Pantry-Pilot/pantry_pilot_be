8f69a18af7d74f994cbe129b605eaf2ec19128c1
Lane Bretschneider <lanebret85@gmail.com>
2023-11-03 20:28:51 -0700

    refactor: rename old recipe poro class to ApiRecipe class

--- app/facades/recipe_facade.rb
+++ app/facades/recipe_facade.rb
@@ -2,12 +2,12 @@ class RecipeFacade
   def self.search(query)
     results = SpoonacularService.new.search_recipes(query)
     recipes = results[:results].map do |recipe_data|
-      Recipe.new(recipe_data)
+      ApiRecipe.new(recipe_data)
     end
   end
 
   def self.find(id)
     detailed_data = SpoonacularService.new.find_recipe(id)
-    Recipe.new(detailed_data)
+    ApiRecipe.new(detailed_data)
   end
 end
