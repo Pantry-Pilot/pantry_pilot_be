00927a30ef3b71fbeee02c3343718dca4beb539b
Lane Bretschneider <lanebret85@gmail.com>
2023-11-04 16:31:43 -0700

    feat: add destroy action

--- app/controllers/api/v1/user_recipes_controller.rb
+++ app/controllers/api/v1/user_recipes_controller.rb
@@ -3,4 +3,9 @@ class Api::V1::UserRecipesController < ApplicationController
     user = User.find(params[:user_id])
     render json: UserRecipesSerializer.new(user.recipes)
   end
+
+  def destroy
+    recipe = Recipe.find(params[:id])
+    recipe.destroy
+  end
 end
