0e4ac17f2463083839007a8c1f7447006135f6ba
Lane Bretschneider <lanebret85@gmail.com>
2023-11-03 20:29:21 -0700

    feat: add create action with private method recipe params

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -16,4 +16,15 @@ class Api::V1::RecipesController < ApplicationController
       render json: { error: "Recipe not found!", status: 421 }, status: :unprocessable_entity
     end
   end
+
+  def create
+    Recipe.create!(recipe_params)
+    UserRecipe.create!(user_id: params[:user_id], recipe_id: params[:id])
+  end
+
+  private
+
+  def recipe_params
+    params.permit(:id, :title, :image, :summary, :instructions, :ingredients)
+  end
 end
