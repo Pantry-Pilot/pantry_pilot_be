a9429d0baf28b20900be9cca23de6e4b77c5d64d
Antoine Aube <aaube3@gmail.com>
2023-11-04 16:59:36 -0600

    error handling with tests, rereorded vcr's unfortuantely.

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -18,9 +18,16 @@ class Api::V1::RecipesController < ApplicationController
   end
 
   def create
+    begin
     user = User.find(user_params[:user_id])
-    recipe = Recipe.create!(recipe_params)
-    UserRecipe.create(user_id: user.id, recipe_id: recipe.id)
+    recipe = Recipe.create(recipe_params)
+    user_recipe= UserRecipe.create(user_id: user.id, recipe_id: recipe.id)
+    # if !recipe.save
+    #   render json: { error: "Recipe not saved!", status: 421 }, status: :unprocessable_entity
+    # end
+    rescue ActiveRecord::RecordNotFound => e
+      render json: { error: e.message, status: 404 }, status: :not_found
+    end
   end
 
   private
