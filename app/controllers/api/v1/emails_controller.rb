e5bed6cc362389cf0879e5bec71fca2af1979fe9
Antoine Aube <aaube3@gmail.com>
2023-11-05 22:03:43 -0700

    feature: send_email_recipe for controlle method

--- app/controllers/api/v1/emails_controller.rb
+++ app/controllers/api/v1/emails_controller.rb
@@ -1,7 +1,12 @@
 class Api::V1::EmailsController < ApplicationController
-  def email_recipe
+  def send_email_recipe
     @recipe = Recipe.email_recipe(params[:recipe_id])
     @recipient = params[:recipient_email]
+    @sender = User.find(params[:sender_id])
+    # require 'pry';binding.pry
     render json: { success: "Email sent to #{@recipient}", status: 200}, status: 200
+    if @recipe
+      RecipeMailer.email_recipe(@recipe, @recipient, @sender).deliver_now
+    end 
   end
 end
