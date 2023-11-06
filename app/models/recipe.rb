0f3738cc5093e74991d5b3b0ead05fac3d184355
Antoine Aube <aaube3@gmail.com>
2023-11-05 20:58:23 -0700

    model method to retrieve and a recipe.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,4 +3,8 @@ class Recipe < ApplicationRecord
   has_many :users, through: :user_recipes
 
   validates :recipe_id, presence: true
+
+  def self.email_recipe(recipe_id)
+    Recipe.where("recipe_id = ?", recipe_id).first
+  end
 end
