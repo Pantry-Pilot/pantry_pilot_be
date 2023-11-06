7658d7f95c30aab714d4237ae4fdf94be297fe30
Antoine Aube <aaube3@gmail.com>
2023-11-06 09:25:56 -0700

    feature: error handling for user not saving recipe before sending.

--- app/controllers/api/v1/emails_controller.rb
+++ app/controllers/api/v1/emails_controller.rb
@@ -3,10 +3,11 @@ class Api::V1::EmailsController < ApplicationController
     @recipe = Recipe.email_recipe(params[:recipe_id])
     @recipient = params[:recipient_email]
     @sender = User.find(params[:sender_id])
-    # require 'pry';binding.pry
-    render json: { success: "Email sent to #{@recipient}", status: 200}, status: 200
     if @recipe
+      render json: { success: "Email sent to #{@recipient}", status: 200}, status: 200
       RecipeMailer.email_recipe(@recipe, @recipient, @sender).deliver_now
+    else 
+      render json: { error: "Email not sent", status: 400}, status: 400
     end 
   end
 end
