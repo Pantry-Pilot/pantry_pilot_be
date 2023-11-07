45a584fff9853ff941c83af6824e108c0a9501ad
Antoine Aube <aaube3@gmail.com>
2023-11-07 16:39:13 -0700

    update controller action in users controller to user welcome email job

--- app/controllers/api/v1/emails_controller.rb
+++ app/controllers/api/v1/emails_controller.rb
@@ -3,10 +3,9 @@ class Api::V1::EmailsController < ApplicationController
     @recipe = Recipe.email_recipe(params[:recipe_id])
     @recipient = params[:recipient_email]
     @sender = User.find(params[:sender_id])
-    # require 'pry';binding.pry
     if @recipe
-      render json: { success: "Email sent to #{@recipient}", status: 200}, status: 200
       RecipeMailer.email_recipe(@recipe, @recipient, @sender).deliver_now
+      render json: { success: "Email sent to #{@recipient}", status: 200}, status: 200
     else 
       render json: { error: "Email not sent", status: 400}, status: 400
     end 
