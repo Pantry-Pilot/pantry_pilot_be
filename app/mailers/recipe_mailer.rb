652030589c4635c29c5f2cc8ded455afeb7cad15
Antoine Aube <aaube3@gmail.com>
2023-11-07 17:41:48 -0700

    remove pry and circleci test

--- app/mailers/recipe_mailer.rb
+++ app/mailers/recipe_mailer.rb
@@ -2,7 +2,6 @@ class RecipeMailer < ApplicationMailer
   def email_recipe(recipe, recipient, sender)
     @recipe = recipe
     @recipient = recipient
-    require 'pry';binding.pry
     @ingredients = recipe.parsed_ingredients
     @sender = User.find(sender.id)
     mail(to: @recipient, subject: "Check out this recipe!")
